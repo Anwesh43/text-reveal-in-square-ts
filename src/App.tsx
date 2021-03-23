8ddd589582e6fc167917c64b7a031eb76c83b59e
Anwesh43 <anweshthecool0@gmail.com>
2021-03-24 02:01:30 +0530

    using TextBlock and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TextBlock from './TextBlock'
+import {useAnimatedScale, useDimension} from './hooks'
 function App() {
+  const {sf, start} = useAnimatedScale()
+  const {w, h} = useDimension()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextBlock w = {w} h = {h} scale = {sf} onClick = {start}  text = "hello">
+      </TextBlock>
     </div>
   );
 }
