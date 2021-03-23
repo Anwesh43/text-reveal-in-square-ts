3610bd0b223e5c409a76ebde2ebd47a6e191c415
Anwesh43 <anweshthecool0@gmail.com>
2021-03-24 01:43:46 +0530

    adding useDimension hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -41,4 +41,29 @@ export const useDimension : Function = () => {
             setH(window.innerHeight)
         }
     }) 
+    return {
+        w, 
+        h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10
+    return {
+        blockStyle() : Object {
+            return {
+                display : 'flex',
+                width : `${size}px`,
+                height: `${size}px`,
+                position: 'absolute',
+                left : `${w / 2 - size / 2}px`,
+                top : `${h / 2 - size / 2}px`,
+                justifyContent: 'center',
+                alignItems: 'center',
+                background: 'orange',
+                color : 'white'
+                fontSize : `${(size * scale) / 3}`,
+            }
+        }
+    }
 }
